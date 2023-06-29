7eb46d7ccf5895e5cd1531f386ceccebed669504
Lucinhell <125667528+ephimeral@users.noreply.github.com>
2023-06-28 21:08:53 -0500

    Update anti-josesito.py
    Removed selenium and bs4, instead usesrequests.html

--- anti-josesito.py
+++ anti-josesito.py
@@ -1,24 +1,46 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
 import discord
 from discord.ext import commands
+import asyncio
 import random
+from requests_html import AsyncHTMLSession
+import asyncio
+import itertools
 
 class MyBot(commands.Bot):
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        imagenes = None
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.imagenes = []
 
-    async def setup_hook(self):
-        self.imagenes = await scrape()
+	async def setup_hook(self):
+		await self.scraper()
 
-    async def on_ready(self):
-        print(f"Logging in as: {self.user}")
+	async def on_ready(self):
+		print(f"Logging in as: {self.user}")
+
+	async def get_post_link(self, pid):
+		r = await self.asession.get(f'https://rule34.xxx/index.php?page=post&s=list&tags=lt._john_llama&pid={pid}')
+		link_images = [link.attrs['href'] for link in r.html.find('.image-list a')]
+		return link_images
+
+	async def get_image_link(self, url):
+		url = f"https://rule34.xxx{url}"
+
+		r = await self.asession.get(url)
+		imagen = r.html.find('.flexi img')
+		if imagen:
+			imagen = imagen[0].attrs['src']
+			self.imagenes.append(imagen)
+
+	async def scraper(self):
+		self.asession = AsyncHTMLSession()
+		pids = [0, 42, 84, 126, 168]  
+		tasks = [self.get_post_link(pid) for pid in pids]  
+		post_links_nested = await asyncio.gather(*tasks) 
+		post_links = list(itertools.chain.from_iterable(post_links_nested)) 
+		tasks = [self.get_image_link(url) for url in post_links]
+		await asyncio.gather(*tasks)
+
 
 
 
@@ -26,47 +48,15 @@ client = MyBot(command_prefix=commands.when_mentioned_or("jj"),intents=discord.I
                       case_insensitive=True, activity=discord.Game(name="con los pezones de Jhon Llama"))
 client.remove_command('help')
 
+
+
 @client.event
 async def on_message(message):
 	user = message.author
+	if user.bot:
+		return
 
-	await user.send(client.imagenes[random.randint(0,len(client.imagenes) - 1)])
-
-async def scrape():
-	# Configurar firefox sin interfaz
-	chrome_options = Options()
-	chrome_options.add_argument("--headless")
-	chrome_options.add_argument("--log-level=3")
-	driver = webdriver.Chrome(options=chrome_options)
-
-	driver.get("https://rule34.xxx/index.php?page=post&s=list&tags=lt._john_llama")
-
-	# Esperar a el tag 'image-list'
-	wait = WebDriverWait(driver, 2)
-	element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'image-list')))
-
-	soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-	image_links = []
-
-	# Hacer una lista con el link de los posts de las imagenes
-	for element in soup.find(class_='image-list').find_all('a'):
-	    image_link = element.get('href')
-	    url = f"https://rule34.xxx{image_link}"
-	    image_links.append(url)
-
-	imagenes = []
-	# Iterar en cada link y sacar el link de la imagen
-	for url in image_links:
-	    driver.get(url)
-	    image_soup = BeautifulSoup(driver.page_source, 'html.parser')
-	    imagen = image_soup.find(class_='flexi').find('img')
-
-	    if imagen:
-	        imagenes.append(imagen.get('src'))
-
-	driver.quit()
-	return imagenes
-
+	if user.id == 180858106761314305:
+		await user.send(client.imagenes[random.randint(0,len(client.imagenes) - 1)])
 
 client.run(TOKEN)
